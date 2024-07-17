67c1f9830f366e26b670f89e3a899384a01c9cfa
Alidhillong <ahaiderctn@gmail.com>
2024-07-17 12:53:03 +0500

    Add files via upload

---
+++ order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'History of Asia',
+        productNumber: '145',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'copmuters science',
+        productNumber: '146',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'structure of atoms',
+        productNumber: '147',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+     {
+        productName: 'Maths logic',
+        productNumber: '148',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+     {
+        productName: 'Biology concepts',
+        productNumber: '149',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+     {
+        productName: 'Main course of medical',
+        productNumber: '149',
+        paymentStatus: 'Due',
+        status: 'Active'
+    },
+     {
+        productName: 'Ai books ',
+        productNumber: '150',
+        paymentStatus: 'refunded',
+        status: 'Pending'
+    },
+]
